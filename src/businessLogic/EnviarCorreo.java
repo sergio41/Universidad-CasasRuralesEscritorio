629c8a708f1f2440647b7bd62e990c9468c32c6d
urko93 <mportatila@mportatila-P150HMx>
2013-03-31 13:09:17 +0200

    correo

---
+++ src/businessLogic/EnviarCorreo.java
@@ -0,0 +1,53 @@
+package businessLogic;
+ 
+import java.util.Date;
+import java.util.Properties;
+ 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+ 
+public class EnviarCorreo {
+ 
+ public static void main(String[] args) {
+  String servidorSMTP = "smtp.gmail.com";
+  String puerto = "587";
+  String usuario = "villatripasdearribacr@gmail.com";
+  String password = "villavilla";
+   
+  String destino = "urko.larm@gmail.com";
+  String asunto = "Prueba!";
+  String mensaje = "Este es un mensaje de prueba.";
+ 
+  Properties props = new Properties();
+ 
+  props.put("mail.debug", "false");
+  props.put("mail.smtp.auth", true);
+  props.put("mail.smtp.starttls.enable", true);
+  props.put("mail.smtp.host", servidorSMTP);
+  props.put("mail.smtp.port", puerto);
+ 
+  Session session = Session.getInstance(props, null);
+ 
+  try {
+   MimeMessage message = new MimeMessage(session);
+   message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+     destino));
+   message.setSubject(asunto);
+   message.setSentDate(new Date());
+   message.setText(mensaje);
+    
+   Transport tr = session.getTransport("smtp");
+   tr.connect(servidorSMTP, usuario, password);
+   message.saveChanges();   
+   tr.sendMessage(message, message.getAllRecipients());
+   tr.close();
+    
+  } catch (MessagingException e) {
+   e.printStackTrace();
+  }
+ }
+}
