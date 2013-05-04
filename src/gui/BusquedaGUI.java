12dfe9e9577cdd096442128ba40255a03efbf516
sergio41 <sergiorr41@gmail.com>
2013-05-04 16:23:46 +0200

    busquedas a ver si se sube esto ya 1º version no busca aun

--- src/gui/BusquedaGUI.java
+++ src/gui/BusquedaGUI.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.Vector;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
@@ -20,10 +22,21 @@ import domain.RuralHouse;
 
 public class BusquedaGUI extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private JTextField textCity;
 	private DefaultComboBoxModel<String> modeloEC = new DefaultComboBoxModel<String>();
 	private JButton btnSalvar;
-
+	private DefaultComboBoxModel<String> modeloCity = new DefaultComboBoxModel<String>();
+	private DefaultComboBoxModel<String> modeloBanos = new DefaultComboBoxModel<String>();
+	private DefaultComboBoxModel<String> modeloDorm = new DefaultComboBoxModel<String>();
+	private DefaultComboBoxModel<String> modeloCocin = new DefaultComboBoxModel<String>();
+	private DefaultComboBoxModel<String> modeloSala = new DefaultComboBoxModel<String>();
+	private DefaultComboBoxModel<String> modeloParkin = new DefaultComboBoxModel<String>();
+	private JComboBox<String> comboBanos;
+	private JComboBox<String> comboCocinas;
+	private JComboBox<String> comboHabita;
+	private JComboBox<String> comboEstar;
+	private JComboBox<String> comboPark;
+	private JComboBox<String> comboCity;
+	
 	public BusquedaGUI() {
 		setLayout(null);
 		
@@ -44,13 +57,6 @@ public class BusquedaGUI extends JPanel {
 		label_7.setFont(new Font("Dialog", Font.PLAIN, 21));
 		label_7.setBounds(256, 331, 183, 34);
 		add(label_7);
-		
-		textCity = new JTextField();
-		textCity.setText("");
-		textCity.setEnabled(false);
-		textCity.setColumns(10);
-		textCity.setBounds(456, 127, 128, 33);
-		add(textCity);
 		
 		JLabel label = new JLabel("N\u00BA Cocinas*:");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -100,25 +106,36 @@ public class BusquedaGUI extends JPanel {
 		btnSalvar.setBounds(694, 225, 124, 45);
 		add(btnSalvar);
 		
-		JComboBox comboBanos = new JComboBox();
+		comboBanos = new JComboBox<String>();
 		comboBanos.setBounds(456, 176, 128, 27);
+		comboBanos.setModel(modeloBanos);
 		add(comboBanos);
 		
-		JComboBox comboCocinas = new JComboBox();
+		comboCocinas = new JComboBox<String>();
 		comboCocinas.setBounds(456, 217, 128, 27);
+		comboCocinas.setModel(modeloCocin);
 		add(comboCocinas);
 		
-		JComboBox comboHabita = new JComboBox();
+		comboHabita = new JComboBox<String>();
 		comboHabita.setBounds(456, 255, 128, 27);
+		comboHabita.setModel(modeloDorm);
 		add(comboHabita);
 		
-		JComboBox comboEstar = new JComboBox();
+		comboEstar = new JComboBox<String>();
 		comboEstar.setBounds(456, 294, 128, 27);
+		comboEstar.setModel(modeloSala);
 		add(comboEstar);
 		
-		JComboBox comboPark = new JComboBox();
+		comboPark = new JComboBox<String>();
 		comboPark.setBounds(456, 331, 128, 27);
+		comboPark.setModel(modeloParkin);
 		add(comboPark);
+		
+		
+		comboCity = new JComboBox<String>();
+		comboCity.setBounds(456, 131, 128, 27);
+		comboCity.setModel(modeloCity);
+		add(comboCity);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(getClass().getResource("/imagenes/fondoAbajo.jpg")));
@@ -135,18 +152,40 @@ public class BusquedaGUI extends JPanel {
 	}
 
 	private void inicializarCampos() {
-		/*modeloEC.addElement("Nueva Casa Rural");
 		ApplicationFacadeInterface facade = Start.getBusinessLogic();
-		java.util.Iterator<RuralHouse> i;
 		try {
-			i = facade.getOwner().getRuralHouses().iterator();
-			while (i.hasNext()){
-				modeloEC.addElement(Integer.toString(i.next().getHouseNumber()));
+			Vector<RuralHouse> vectorCasas = facade.getAllRuralHouses();
+			for(int i=0; i<vectorCasas.size();i++){
+				if(!estaCity(vectorCasas.get(i).getCity())){
+					modeloCity.addElement(vectorCasas.get(i).getCity());
+				}
+				if(!estaCocina(Integer.toString(vectorCasas.get(i).getKitchen()))){
+					modeloCocin.addElement(Integer.toString(vectorCasas.get(i).getKitchen()));
+				}
+				if(!estaBanos(Integer.toString(vectorCasas.get(i).getBaths()))){
+					modeloBanos.addElement(Integer.toString(vectorCasas.get(i).getBaths()));
+				}
+				if(!estaSala(Integer.toString(vectorCasas.get(i).getLiving()))){
+					modeloSala.addElement(Integer.toString(vectorCasas.get(i).getLiving()));
+				}
+				if(!estaDorm(Integer.toString(vectorCasas.get(i).getRooms()))){
+					modeloDorm.addElement(Integer.toString(vectorCasas.get(i).getRooms()));
+				}
+				if(!estaPark(Integer.toString(vectorCasas.get(i).getPark()))){
+					modeloParkin.addElement(Integer.toString(vectorCasas.get(i).getPark()));
+				}
 			}
-			enaDis(false);
+			comboBanos.setSelectedIndex(-1);
+			comboCity.setSelectedIndex(-1);
+			comboCocinas.setSelectedIndex(-1);
+			comboEstar.setSelectedIndex(-1);
+			comboHabita.setSelectedIndex(-1);
+			comboPark.setSelectedIndex(-1);
+			btnSalvar.setEnabled(true);
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 	}
 	
 
@@ -157,4 +196,60 @@ public class BusquedaGUI extends JPanel {
 	public void setModeloEC(DefaultComboBoxModel<String> modeloEC) {
 		this.modeloEC = modeloEC;
 	}
+	
+	public boolean estaCity(String s){
+		for (int j = 0; j<modeloCity.getSize();j++){
+			System.out.println(j+modeloCity.getElementAt(j)+" "+s);
+			if(modeloCity.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
+	public boolean estaBanos(String s){
+		for (int j = 0; j<modeloBanos.getSize();j++){
+			if(modeloBanos.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
+	public boolean estaCocina(String s){
+		for (int j = 0; j<modeloCocin.getSize();j++){
+			if(modeloCocin.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
+	public boolean estaSala(String s){
+		for (int j = 0; j<modeloSala.getSize();j++){
+			if(modeloSala.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
+	public boolean estaDorm(String s){
+		for (int j = 0; j<modeloDorm.getSize();j++){
+			if(modeloDorm.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
+	public boolean estaPark(String s){
+		for (int j = 0; j<modeloParkin.getSize();j++){
+			if(modeloParkin.getElementAt(j).compareTo(s)==0){
+				return true;
+			}
+		}
+		return false;		
+	}
+	
 }
