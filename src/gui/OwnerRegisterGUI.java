a302af6149dc6671c952b6dcc9cb620a26c14c22
urko93 <urkol_000@mportatila>
2013-05-04 19:23:53 +0200

    Interfaz owner

--- src/gui/OwnerRegisterGUI.java
+++ src/gui/OwnerRegisterGUI.java
@@ -16,10 +16,14 @@ import businessLogic.ApplicationFacadeInterface;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.text.ParseException;
 import java.util.Vector;
 
 import javax.swing.JRadioButton;
+import javax.swing.text.MaskFormatter;
+
 import domain.Owner;
+import javax.swing.JFormattedTextField;
 
 public class OwnerRegisterGUI extends JPanel {
 	/**
@@ -31,14 +35,13 @@ public class OwnerRegisterGUI extends JPanel {
 	private static JTextField textIdioma1;
 	private static JTextField textIdioma2;
 	private static JTextField textIdioma4;
-	private static JTextField textCuentaBancaria;
 	private static JButton buttonGuardar;
 	
 	private static JComboBox<String> comboMoneda;
 	private static JRadioButton rdbtnProfesional = new JRadioButton("Profesional");
 	private static JRadioButton rdbtnParticular = new JRadioButton("Particular");
 	private static DefaultComboBoxModel<String> modeloMon = new DefaultComboBoxModel<String>();
-	ApplicationFacadeInterface facade = Start.getBusinessLogic();
+	private static JFormattedTextField textCuentaBancaria;
 	
 	/**
 	 * Create the panel.
@@ -48,7 +51,7 @@ public class OwnerRegisterGUI extends JPanel {
 		buttonGuardar = new JButton("Registrar");
 		buttonGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//ApplicationFacadeInterface facade = Start.getBusinessLogic();
+				ApplicationFacadeInterface facade = Start.getBusinessLogic();
 				String cuenta= "";
 				String tipo = "";
 				String profes= "";
@@ -83,18 +86,18 @@ public class OwnerRegisterGUI extends JPanel {
 		});
 		buttonGuardar.setForeground(Color.BLUE);
 		buttonGuardar.setFont(new Font("Dialog", Font.PLAIN, 21));
-		buttonGuardar.setBounds(508, 295, 124, 45);
+		buttonGuardar.setBounds(650, 394, 124, 45);
 		add(buttonGuardar);
 		
 		JLabel label = new JLabel("Los campos marcados con * son obligatorios");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
 		label.setForeground(Color.RED);
 		label.setFont(new Font("Dialog", Font.PLAIN, 18));
-		label.setBounds(130, 306, 366, 34);
+		label.setBounds(272, 400, 366, 34);
 		add(label);
 		
 		comboMoneda = new JComboBox<String>();
-		comboMoneda.setBounds(248, 261, 109, 34);
+		comboMoneda.setBounds(360, 333, 109, 34);
 		comboMoneda.setModel(modeloMon);
 		modeloMon.addElement("");
 		modeloMon.addElement("€");
@@ -105,71 +108,79 @@ public class OwnerRegisterGUI extends JPanel {
 		JLabel label_1 = new JLabel("Moneda:*");
 		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_1.setFont(new Font("Dialog", Font.PLAIN, 21));
-		label_1.setBounds(36, 265, 161, 34);
+		label_1.setBounds(153, 333, 161, 34);
 		add(label_1);
 		
 		JLabel label_2 = new JLabel("Profesi\u00F3n:*");
 		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_2.setFont(new Font("Dialog", Font.PLAIN, 21));
-		label_2.setBounds(36, 218, 161, 34);
+		label_2.setBounds(153, 272, 161, 34);
 		add(label_2);
 		
 		textProfesion = new JTextField();
 		textProfesion.setColumns(10);
-		textProfesion.setBounds(252, 218, 366, 34);
+		textProfesion.setBounds(360, 272, 414, 34);
 		add(textProfesion);
 		
 		textIdioma3 = new JTextField();
 		textIdioma3.setColumns(10);
-		textIdioma3.setBounds(252, 171, 161, 34);
+		textIdioma3.setBounds(360, 211, 161, 34);
 		add(textIdioma3);
 		
 		textIdioma1 = new JTextField();
 		textIdioma1.setColumns(10);
-		textIdioma1.setBounds(252, 128, 161, 34);
+		textIdioma1.setBounds(360, 150, 161, 34);
 		add(textIdioma1);
 		
 		textIdioma2 = new JTextField();
 		textIdioma2.setColumns(10);
-		textIdioma2.setBounds(457, 128, 161, 34);
+		textIdioma2.setBounds(613, 150, 161, 34);
 		add(textIdioma2);
 		
 		textIdioma4 = new JTextField();
 		textIdioma4.setColumns(10);
-		textIdioma4.setBounds(457, 171, 161, 34);
+		textIdioma4.setBounds(613, 211, 161, 34);
 		add(textIdioma4);
 		
 		JRadioButton rdbtnProfesional = new JRadioButton("Profesional");
 		rdbtnProfesional.setOpaque(false);
-		rdbtnProfesional.setBounds(457, 81, 127, 34);
+		rdbtnProfesional.setBounds(647, 89, 127, 34);
 		add(rdbtnProfesional);
 		
 		JRadioButton rdbtnParticular = new JRadioButton("Particular");
 		rdbtnParticular.setOpaque(false);
-		rdbtnParticular.setBounds(252, 81, 127, 34);
+		rdbtnParticular.setBounds(360, 82, 127, 34);
 		add(rdbtnParticular);
 		
 		JLabel label_3 = new JLabel("Usted es:*");
 		label_3.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_3.setFont(new Font("Dialog", Font.PLAIN, 21));
-		label_3.setBounds(36, 81, 161, 34);
+		label_3.setBounds(153, 82, 161, 34);
 		add(label_3);
 		
 		JLabel label_4 = new JLabel("Idiomas hablados:*");
 		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_4.setFont(new Font("Dialog", Font.PLAIN, 21));
-		label_4.setBounds(36, 128, 161, 34);
+		label_4.setBounds(102, 150, 212, 34);
 		add(label_4);
 		
 		JLabel label_5 = new JLabel("Cuenta bancar\u00EDa:*");
 		label_5.setHorizontalAlignment(SwingConstants.RIGHT);
 		label_5.setFont(new Font("Dialog", Font.PLAIN, 21));
-		label_5.setBounds(36, 34, 161, 34);
+		label_5.setBounds(94, 25, 220, 34);
 		add(label_5);
 		
-		textCuentaBancaria = new JTextField();
-		textCuentaBancaria.setColumns(10);
-		textCuentaBancaria.setBounds(252, 34, 366, 34);
+		MaskFormatter modeloCB = null;
+		try {
+			modeloCB = new MaskFormatter("####-####-##-##########");
+			modeloCB.setPlaceholderCharacter('_');
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		textCuentaBancaria = new JFormattedTextField(modeloCB);
+		textCuentaBancaria.setBounds(360, 25, 414, 34);
 		add(textCuentaBancaria);
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(getClass().getResource("/imagenes/fondoAbajo.jpg")));
@@ -182,9 +193,10 @@ public class OwnerRegisterGUI extends JPanel {
 	
 	@SuppressWarnings("deprecation")
 	private void inicializarCampos(){
-		//ApplicationFacadeInterface facade = Start.getBusinessLogic();
+		ApplicationFacadeInterface facade = Start.getBusinessLogic();
 		Owner ow;
 		try {
+			
 			ow = facade.getOwner();
 			if (ow!=null){
 				textCuentaBancaria.enable(false);
