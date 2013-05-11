13f99479ad41526d276f488b62225f60ef498ec9
urko93 <urkol_000@mportatila>
2013-05-11 13:55:29 +0200

    Ver fotos

--- src/gui/VerFotos.java
+++ src/gui/VerFotos.java
@@ -15,8 +15,9 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
-public class verFotos extends JFrame {
+public class VerFotos extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private Vector<Image> images;
@@ -24,9 +25,9 @@ public class verFotos extends JFrame {
 	private DefaultComboBoxModel<Integer> modeloCombo = new DefaultComboBoxModel<Integer>();
 	private JLabel label_1 = new JLabel("");
 	private JLabel label_2 = new JLabel("");
-	private ImageIcon imagenDefecto = new ImageIcon(verFotos.class.getResource("/localData/casaDefault.png"));
-	private JLabel lblNewLabel;
-	private Image aux = (new ImageIcon(verFotos.class.getResource("/localData/casaDefault.png"))).getImage();
+	//private ImageIcon imagenDefecto = new ImageIcon(verFotos.class.getResource("/localData/casaDefault.png"));
+	private JLabel lblNewLabel = new JLabel();
+	private Image aux = (new ImageIcon(VerFotos.class.getResource("/localData/casaDefault.png"))).getImage();
 	/**
 	 * Launch the application.
 	 */
@@ -34,7 +35,7 @@ public class verFotos extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					verFotos frame = new verFotos(null);
+					VerFotos frame = new VerFotos(null);
 					frame.setVisible(true);
 					
 				} catch (Exception e) {
@@ -47,12 +48,14 @@ public class verFotos extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public verFotos(Vector<Image> imagenes) {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(verFotos.class.getResource("/localData/casaDefault.png")));
+	public VerFotos(Vector<Image> imagenes) {
+		setBackground(new Color(0, 206, 209));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(VerFotos.class.getResource("/localData/casaDefault.png")));
 		setTitle("Ver fotos Casa Rural");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 438, 497);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 206, 209));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -60,7 +63,7 @@ public class verFotos extends JFrame {
 		comboBox = new JComboBox<Integer>();
 		comboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				cargarImagen(0);
+				cargarImagen(comboBox.getSelectedIndex());
 			}
 		});
 		comboBox.setModel(modeloCombo);
@@ -73,10 +76,10 @@ public class verFotos extends JFrame {
 		label_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				if (comboBox.getSelectedIndex() != comboBox.getItemCount())	comboBox.setSelectedIndex(comboBox.getSelectedIndex()+1);
+				if (comboBox.getSelectedIndex() != comboBox.getItemCount()-1) comboBox.setSelectedIndex(comboBox.getSelectedIndex()+1);
 			}
 		});
-		label_1.setIcon(new ImageIcon(verFotos.class.getResource("/localData/flechaDer.png")));
+		label_1.setIcon(new ImageIcon(VerFotos.class.getResource("/localData/flechaDer.png")));
 		label_1.setBounds(327, 13, 50, 50);
 		contentPane.add(label_1);
 		
@@ -87,29 +90,30 @@ public class verFotos extends JFrame {
 				if (comboBox.getSelectedIndex() !=0) comboBox.setSelectedIndex(comboBox.getSelectedIndex()-1);
 			}
 		});
-		label_2.setIcon(new ImageIcon(verFotos.class.getResource("/localData/flechaIzq.png")));
+		label_2.setIcon(new ImageIcon(VerFotos.class.getResource("/localData/flechaIzq.png")));
 		label_2.setBounds(40, 13, 50, 50);
 		contentPane.add(label_2);
 		
-		lblNewLabel = new JLabel("New label");
+		lblNewLabel = new JLabel();
 		lblNewLabel.setBounds(12, 94, 396, 343);
 		contentPane.add(lblNewLabel);
 		
 		images = imagenes;
-		imagenDefecto = new ImageIcon(verFotos.class.getResource("/localData/casaDefault.png"));
 		
 		cargarImagen(0);
 		rellenarComboBox();
 	}
 	
 	public  void cargarImagen(int numero){
-		
+		Image aux2 = aux;
 		if (images != null) {
 			if (images != null && numero >= images.size()) numero = images.size();
-			if (images != null && images.size() != 0)aux = images.elementAt(numero);
+			if (images != null && images.size() != 0)aux2 = images.elementAt(numero);
 		}
-        Image aux1 = aux.getScaledInstance(lblNewLabel.getHeight(), lblNewLabel.getWidth(), java.awt.Image.SCALE_SMOOTH);
-        lblNewLabel.setIcon(new ImageIcon(aux1));
+		if (lblNewLabel.getHeight() != 0 && lblNewLabel.getWidth()!= 0) {
+			Image aux1 = aux2.getScaledInstance(lblNewLabel.getHeight(), lblNewLabel.getWidth(), java.awt.Image.SCALE_SMOOTH);
+			lblNewLabel.setIcon(new ImageIcon(aux1));
+		}
 	}
 	
 	public  void rellenarComboBox(){
@@ -119,7 +123,7 @@ public class verFotos extends JFrame {
 			label_1.setEnabled(false);
 			label_2.setEnabled(false);
 		} else { 
-			for (int i = 1; i<images.size(); i++) modeloCombo.addElement(i);
+			for (int i = 1; i<=images.size(); i++) modeloCombo.addElement(i);
 			comboBox.setEnabled(true);
 			label_1.setEnabled(true);
 			label_2.setEnabled(true);
