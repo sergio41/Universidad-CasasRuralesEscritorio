a202e72684dca2fc71b551b856ca3a6fc5baca36
urko93 <urkol_000@mportatila>
2013-05-11 17:07:17 +0200

    Busqueda cambiada.

--- src/gui/BusquedaGUI.java
+++ src/gui/BusquedaGUI.java
@@ -16,6 +16,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 import businessLogic.ApplicationFacadeInterface;
 import domain.Book;
@@ -26,10 +27,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
-import com.toedter.calendar.JDateChooser;
-import javax.swing.JCheckBox;
-import java.awt.event.ItemListener;
-import java.awt.event.ItemEvent;
+import javax.swing.JSpinner;
 
 public class BusquedaGUI extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -41,13 +39,15 @@ public class BusquedaGUI extends JPanel {
 	private DefaultComboBoxModel<String> modeloCocin = new DefaultComboBoxModel<String>();
 	private DefaultComboBoxModel<String> modeloSala = new DefaultComboBoxModel<String>();
 	private DefaultComboBoxModel<String> modeloParkin = new DefaultComboBoxModel<String>();
-	private JComboBox<String> comboBanos;
-	private JComboBox<String> comboCocinas;
-	private JComboBox<String> comboHabita;
-	private JComboBox<String> comboEstar;
-	private JComboBox<String> comboPark;
-	private JComboBox<String> comboCity;
+
+	private JSpinner spinnerAparcamientos;
+	private JSpinner spinnerSalas;
+	private JSpinner spinnerHabitaciones;
+	private JSpinner spinnerCocinas;
+	private JSpinner spinnerBanos;
+	
 	private JTable tableCasas;
+	private JComboBox<String> comboCity;
 	private DefaultTableModel modelTb = new DefaultTableModel(
 			new Object[][] {
 			},
@@ -68,154 +68,35 @@ public class BusquedaGUI extends JPanel {
 	private JTextPane textDescrp;
 	private JLabel lblprop;
 	private JLabel lblTelef;
-	private JCheckBox chckbxNewCheckBox;
-	private JDateChooser dateChooserFin;
-	private JDateChooser dateChooserIni;
 	private JScrollPane scrollPaneOfer;
+	
+
+	private SpinnerNumberModel modeloSpinnerBanos = new SpinnerNumberModel(2, 2, 40, 1);
+	private SpinnerNumberModel modeloSpinnerCocinas = new SpinnerNumberModel(1, 1, 40, 1);
+	private SpinnerNumberModel modeloSpinnerHabitaciones = new SpinnerNumberModel(3, 3, 40, 1);
+	private SpinnerNumberModel modeloSpinnerSala = new SpinnerNumberModel(0, 0, 40, 1);
+	private SpinnerNumberModel modeloSpinnerAparcamiento = new SpinnerNumberModel(0, 0, 40, 1);
 	
 	@SuppressWarnings("serial")
 	public BusquedaGUI() {
 		setLayout(null);
 		
-		JLabel label_5 = new JLabel("N\u00BA Habitaciones*:");
-		label_5.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_5.setFont(new Font("Dialog", Font.BOLD, 11));
-		label_5.setBounds(108, 112, 114, 34);
-		add(label_5);
-		
-		JLabel label_6 = new JLabel("N\u00BA Salas de estar*:");
-		label_6.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_6.setFont(new Font("Dialog", Font.BOLD, 11));
-		label_6.setBounds(98, 142, 124, 34);
-		add(label_6);
-		
-		JLabel label_7 = new JLabel("N\u00BA Aparcamientos*:");
-		label_7.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_7.setFont(new Font("Dialog", Font.BOLD, 11));
-		label_7.setBounds(98, 172, 124, 34);
-		add(label_7);
-		
-		JLabel label = new JLabel("N\u00BA Cocinas*:");
-		label.setHorizontalAlignment(SwingConstants.RIGHT);
-		label.setFont(new Font("Dialog", Font.BOLD, 11));
-		label.setBounds(128, 82, 94, 34);
-		add(label);
-		
-		JLabel label_1 = new JLabel("N\u00BA Ba\u00F1os*:");
-		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_1.setFont(new Font("Dialog", Font.BOLD, 11));
-		label_1.setBounds(139, 53, 83, 34);
-		add(label_1);
-		
-		JLabel label_2 = new JLabel("Ciudad*:");
-		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
-		label_2.setFont(new Font("Times New Roman", Font.BOLD, 15));
-		label_2.setBounds(137, 23, 83, 34);
-		add(label_2);
-		
 		btnSalvar = new JButton("Buscar");
 		btnSalvar.addActionListener(new ActionListener() {
-			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent arg0) {
 				try {
-					if(chckbxNewCheckBox.isSelected()){
-						String ciudad =(String) comboCity.getSelectedItem();
-						int Banos;
-						int Habita;
-						int Cocina;
-						int Estar;
-						int Park;
-						if(comboBanos.getSelectedIndex()==-1){Banos=0;
-						}else{Banos = Integer.parseInt((String) comboBanos.getSelectedItem());}
-						
-						if(comboHabita.getSelectedIndex()==-1){Habita=0;
-						}else{Habita = Integer.parseInt((String) comboHabita.getSelectedItem());}
-						
-						if(comboCocinas.getSelectedIndex()==-1){Cocina=0;
-						}else{Cocina = Integer.parseInt((String) comboCocinas.getSelectedItem());}
-						
-						if(comboEstar.getSelectedIndex()==-1){Estar=0;
-						}else{Estar = Integer.parseInt((String) comboEstar.getSelectedItem());}
-						
-						if(comboPark.getSelectedIndex()==-1){Park=0;
-						}else{Park = Integer.parseInt((String) comboPark.getSelectedItem());}
-					
-						actualizarTabla(ciudad,Banos,Habita,Cocina,Estar,Park);
-					}else{
-						String ciudad =(String) comboCity.getSelectedItem();
-						int Banos;
-						int Habita;
-						int Cocina;
-						int Estar;
-						int Park;
-						if(comboBanos.getSelectedIndex()==-1){Banos=-1;
-						}else{Banos = Integer.parseInt((String) comboBanos.getSelectedItem());}
-						
-						if(comboHabita.getSelectedIndex()==-1){Habita=-1;
-						}else{Habita = Integer.parseInt((String) comboHabita.getSelectedItem());}
-						
-						if(comboCocinas.getSelectedIndex()==-1){Cocina=-1;
-						}else{Cocina = Integer.parseInt((String) comboCocinas.getSelectedItem());}
-						
-						if(comboEstar.getSelectedIndex()==-1){Estar=-1;
-						}else{Estar = Integer.parseInt((String) comboEstar.getSelectedItem());}
-						
-						if(comboPark.getSelectedIndex()==-1){Park=-1;
-						}else{Park = Integer.parseInt((String) comboPark.getSelectedItem());}
-						
-						Date ini = dateChooserIni.getDate();
-						Date ini2 = new Date(ini.getYear(), ini.getMonth(), ini.getDate(), 0, 0);
-						Date fin = dateChooserFin.getDate();
-						Date fin2 = new Date(fin.getYear(), fin.getMonth(), fin.getDate(), 0, 0);
-						actualizarTabla(ciudad,Banos,Habita,Cocina,Estar,Park,ini2, fin2);
-					}
+					String ciudad =(String) comboCity.getSelectedItem();
+					actualizarTabla(ciudad,(int)spinnerBanos.getValue(),(int)spinnerHabitaciones.getValue(),(int)spinnerCocinas.getValue(),(int)spinnerSalas.getValue(),(int)spinnerAparcamientos.getValue());
 				} catch (Exception e) {
 					javax.swing.JOptionPane.showMessageDialog(null,e.toString(), "Mal....",javax.swing.JOptionPane.ERROR_MESSAGE);
 				}
 			}
 		});
 		btnSalvar.setForeground(Color.BLUE);
-		btnSalvar.setFont(new Font("Dialog", Font.PLAIN, 21));
+		btnSalvar.setFont(new Font("Dialog", Font.BOLD, 19));
 		btnSalvar.setEnabled(false);
-		btnSalvar.setBounds(108, 225, 259, 27);
+		btnSalvar.setBounds(201, 265, 163, 34);
 		add(btnSalvar);
-		
-		comboBanos = new JComboBox<String>();
-		comboBanos.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboBanos.setBounds(239, 57, 128, 27);
-		comboBanos.setModel(modeloBanos);
-		add(comboBanos);
-		
-		comboCocinas = new JComboBox<String>();
-		comboCocinas.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboCocinas.setBounds(239, 87, 128, 27);
-		comboCocinas.setModel(modeloCocin);
-		add(comboCocinas);
-		
-		comboHabita = new JComboBox<String>();
-		comboHabita.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboHabita.setBounds(239, 117, 128, 27);
-		comboHabita.setModel(modeloDorm);
-		add(comboHabita);
-		
-		comboEstar = new JComboBox<String>();
-		comboEstar.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboEstar.setBounds(239, 147, 128, 27);
-		comboEstar.setModel(modeloSala);
-		add(comboEstar);
-		
-		comboPark = new JComboBox<String>();
-		comboPark.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboPark.setBounds(239, 177, 128, 27);
-		comboPark.setModel(modeloParkin);
-		add(comboPark);
-		
-		
-		comboCity = new JComboBox<String>();
-		comboCity.setFont(new Font("Dialog", Font.BOLD, 11));
-		comboCity.setBounds(239, 27, 128, 27);
-		comboCity.setModel(modeloCity);
-		add(comboCity);
 		
 		tableCasas = new JTable(){
 	        public boolean isCellEditable(int rowIndex, int vColIndex) {
@@ -247,7 +128,7 @@ public class BusquedaGUI extends JPanel {
 		add(tableCasas);
 		
 		scrollPane = new JScrollPane(tableCasas, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setBounds(377, 68, 571, 184);
+		scrollPane.setBounds(379, 13, 621, 239);
 		add(scrollPane);
 		
 		tableOfertas = new JTable();
@@ -256,15 +137,17 @@ public class BusquedaGUI extends JPanel {
 		add(tableOfertas);
 		
 		scrollPaneOfer = new JScrollPane(tableOfertas, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		scrollPaneOfer.setBounds(626, 281, 318, 67);
+		scrollPaneOfer.setBounds(379, 319, 423, 132);
 		add(scrollPaneOfer);
 		
 		JLabel label_3 = new JLabel("Descripci\u00F3n:");
 		label_3.setFont(new Font("Tahoma", Font.BOLD, 12));
-		label_3.setBounds(56, 258, 114, 34);
+		label_3.setBounds(33, 310, 114, 34);
 		add(label_3);
 		
 		btnNewButton = new JButton("Reservar\r\n");
+		btnNewButton.setForeground(Color.BLUE);
+		btnNewButton.setFont(new Font("Dialog", Font.BOLD, 19));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				ApplicationFacadeInterface facade = Start.getBusinessLogic();
@@ -289,80 +172,115 @@ public class BusquedaGUI extends JPanel {
 				
 			}
 		});	
-		btnNewButton.setBounds(781, 397, 163, 34);
+		btnNewButton.setBounds(835, 417, 163, 34);
 		add(btnNewButton);
 		
 		textDescrp = new JTextPane();
-		textDescrp.setBounds(66, 286, 301, 62);
+		textDescrp.setBounds(33, 346, 301, 105);
 		textDescrp.setEditable(false);
 		add(textDescrp);
 		
 		JLabel lblNewLabel_1 = new JLabel("Propietario:");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblNewLabel_1.setBounds(56, 359, 74, 21);
+		lblNewLabel_1.setBounds(623, 278, 74, 21);
 		add(lblNewLabel_1);
 		
 		lblprop = new JLabel("");
 		lblprop.setForeground(new Color(0, 128, 0));
 		lblprop.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblprop.setBounds(133, 359, 234, 21);
+		lblprop.setBounds(709, 278, 273, 21);
 		add(lblprop);
 		
 		JLabel lblTelefono = new JLabel("Tel\u00E9fono:");
 		lblTelefono.setFont(new Font("Tahoma", Font.BOLD, 12));
-		lblTelefono.setBounds(56, 404, 74, 14);
+		lblTelefono.setBounds(379, 278, 74, 21);
 		add(lblTelefono);
 		
 		lblTelef = new JLabel("");
 		lblTelef.setForeground(new Color(0, 128, 0));
-		lblTelef.setBounds(128, 404, 239, 14);
+		lblTelef.setBounds(463, 278, 104, 21);
 		add(lblTelef);
 		
-		JButton btnImg = new JButton("Ver imagenes");
-		btnImg.setBounds(373, 367, 249, 23);
-		add(btnImg);
+		JLabel label = new JLabel("N\u00BA min. aparcamientos*:");
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		label.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label.setBounds(10, 225, 179, 27);
+		add(label);
 		
-		dateChooserIni = new JDateChooser();
-		dateChooserIni.setEnabled(true);
-		dateChooserIni.setDateFormatString("yyyy-MM-dd");
-		dateChooserIni.setBounds(551, 30, 149, 21);
-		add(dateChooserIni);
+		spinnerAparcamientos = new JSpinner();
+		spinnerAparcamientos.setModel(modeloSpinnerAparcamiento);
+		spinnerAparcamientos.setBounds(201, 225, 67, 27);
+		add(spinnerAparcamientos);
 		
-		dateChooserFin = new JDateChooser();
-		dateChooserFin.setEnabled(true);
-		dateChooserFin.setDateFormatString("yyyy-MM-dd");
-		dateChooserFin.setBounds(799, 30, 149, 22);
-		add(dateChooserFin);
+		spinnerSalas = new JSpinner();
+		spinnerSalas.setModel(modeloSpinnerSala);
+		spinnerSalas.setBounds(201, 182, 67, 27);
+		add(spinnerSalas);
 		
-		JLabel lblFechaInicio = new JLabel("Fecha Inicio:");
-		lblFechaInicio.setFont(new Font("Dialog", Font.BOLD, 11));
-		lblFechaInicio.setBounds(480, 30, 74, 21);
-		add(lblFechaInicio);
+		JLabel label_1 = new JLabel("N\u00BA min. salas de estar*:");
+		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_1.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label_1.setBounds(23, 182, 166, 27);
+		add(label_1);
 		
-		JLabel lblFechaFin = new JLabel("Fecha Fin:");
-		lblFechaFin.setFont(new Font("Dialog", Font.BOLD, 11));
-		lblFechaFin.setBounds(741, 30, 74, 21);
-		add(lblFechaFin);
+		JLabel label_2 = new JLabel("N\u00BA min. habitaciones*:");
+		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_2.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label_2.setBounds(33, 139, 156, 27);
+		add(label_2);
 		
-		chckbxNewCheckBox = new JCheckBox("Sin fecha");
-		chckbxNewCheckBox.addItemListener(new ItemListener() {
-			public void itemStateChanged(ItemEvent arg0) {
-				if(chckbxNewCheckBox.isSelected()){
-					dateChooserIni.setEnabled(false);
-					dateChooserFin.setEnabled(false);
-				}else{
-					dateChooserIni.setEnabled(true);
-					dateChooserFin.setEnabled(true);
-				}	
-			}
-		});
-		chckbxNewCheckBox.setOpaque(false);
-		chckbxNewCheckBox.setBounds(377, 30, 97, 23);
-		add(chckbxNewCheckBox);
+		spinnerHabitaciones = new JSpinner();
+		spinnerHabitaciones.setModel(modeloSpinnerHabitaciones);
+		spinnerHabitaciones.setBounds(201, 139, 67, 27);
+		add(spinnerHabitaciones);
 		
-		JButton buttonMapa = new JButton("Ver mapa");
-		buttonMapa.setBounds(373, 397, 249, 23);
-		add(buttonMapa);
+		spinnerCocinas = new JSpinner();
+		spinnerCocinas.setModel(modeloSpinnerCocinas);
+		spinnerCocinas.setBounds(201, 96, 67, 27);
+		add(spinnerCocinas);
+		
+		JLabel label_4 = new JLabel("N\u00BA min. cocinas*:");
+		label_4.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_4.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label_4.setBounds(75, 96, 114, 27);
+		add(label_4);
+		
+		JLabel label_5 = new JLabel("N\u00BA min. ba\u00F1os*:");
+		label_5.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_5.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label_5.setBounds(75, 53, 114, 27);
+		add(label_5);
+		
+		spinnerBanos = new JSpinner();
+		spinnerBanos.setModel(modeloSpinnerBanos);
+		spinnerBanos.setBounds(201, 55, 67, 27);
+		add(spinnerBanos);
+		
+		comboCity = new JComboBox<String>();
+		comboCity.setSelectedIndex(-1);
+		comboCity.setFont(new Font("Dialog", Font.BOLD, 11));
+		comboCity.setBounds(201, 10, 149, 27);
+		add(comboCity);
+		
+		JLabel label_6 = new JLabel("Ciudad*:");
+		label_6.setHorizontalAlignment(SwingConstants.RIGHT);
+		label_6.setFont(new Font("Times New Roman", Font.BOLD, 15));
+		label_6.setBounds(104, 10, 83, 27);
+		add(label_6);
+		
+		JButton button = new JButton("Ver imagenes");
+		button.setForeground(Color.BLUE);
+		button.setFont(new Font("Dialog", Font.BOLD, 19));
+		button.setEnabled(false);
+		button.setBounds(835, 319, 163, 34);
+		add(button);
+		
+		JButton button_1 = new JButton("Ver mapa");
+		button_1.setForeground(Color.BLUE);
+		button_1.setFont(new Font("Dialog", Font.BOLD, 19));
+		button_1.setEnabled(false);
+		button_1.setBounds(835, 370, 163, 34);
+		add(button_1);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(getClass().getResource("/localData/fondoAbajo.jpg")));
@@ -370,33 +288,6 @@ public class BusquedaGUI extends JPanel {
 		add(lblNewLabel);
 		
 		inicializarCampos();
-	}
-
-	private void actualizarTabla(String city, int banos, int habita,
-			int cocina, int estar, int park, Date ini, Date fin) {
-		
-		ApplicationFacadeInterface facade = Start.getBusinessLogic();
-		try {
-			borrarTabla();
-			borrarTablaOferta();
-			Vector<RuralHouse> aux =  facade.casasRuralesDisponibles(ini, fin, city,banos,habita,cocina,estar,park );
-			Iterator<RuralHouse> i = aux.iterator();
-			while (i.hasNext()){
-				Vector<Object>  vector = new Vector<Object>();
-				RuralHouse auxi = i.next();
-				vector.add(auxi.getHouseNumber());
-				vector.add(auxi.getCity());
-				vector.add(auxi.getBaths());
-				vector.add(auxi.getKitchen());
-				vector.add(auxi.getLiving());
-				vector.add(auxi.getRooms());
-				vector.add(auxi.getPark());
-				modelTb.addRow(vector);
-			}
-			ajustarColumnas();
-		} catch (Exception e) {
-			e.getMessage();
-		}
 	}
 
 	private void inicializarCampos() {
@@ -407,32 +298,11 @@ public class BusquedaGUI extends JPanel {
 				if(!estaCity(vectorCasas.get(i).getCity())){
 					modeloCity.addElement(vectorCasas.get(i).getCity());
 				}
-				if(!estaCocina(Integer.toString(vectorCasas.get(i).getKitchen()))){
-					modeloCocin.addElement(Integer.toString(vectorCasas.get(i).getKitchen()));
-				}
-				if(!estaBanos(Integer.toString(vectorCasas.get(i).getBaths()))){
-					modeloBanos.addElement(Integer.toString(vectorCasas.get(i).getBaths()));
-				}
-				if(!estaSala(Integer.toString(vectorCasas.get(i).getLiving()))){
-					modeloSala.addElement(Integer.toString(vectorCasas.get(i).getLiving()));
-				}
-				if(!estaDorm(Integer.toString(vectorCasas.get(i).getRooms()))){
-					modeloDorm.addElement(Integer.toString(vectorCasas.get(i).getRooms()));
-				}
-				if(!estaPark(Integer.toString(vectorCasas.get(i).getPark()))){
-					modeloParkin.addElement(Integer.toString(vectorCasas.get(i).getPark()));
-				}
 			}
 			if(Start.estadoLogin())
 				btnNewButton.setEnabled(true);
 			else
 				btnNewButton.setEnabled(false);
-			comboBanos.setSelectedIndex(-1);
-			comboCity.setSelectedIndex(-1);
-			comboCocinas.setSelectedIndex(-1);
-			comboEstar.setSelectedIndex(-1);
-			comboHabita.setSelectedIndex(-1);
-			comboPark.setSelectedIndex(-1);
 			btnSalvar.setEnabled(true);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -554,49 +424,44 @@ public class BusquedaGUI extends JPanel {
 		}
 		return false;		
 	}
-	
-	public boolean estaBanos(String s){
-		for (int j = 0; j<modeloBanos.getSize();j++){
-			if(modeloBanos.getElementAt(j).compareTo(s)==0){
-				return true;
-			}
-		}
-		return false;		
+
+	public DefaultComboBoxModel<String> getModeloParkin() {
+		return modeloParkin;
 	}
-	
-	public boolean estaCocina(String s){
-		for (int j = 0; j<modeloCocin.getSize();j++){
-			if(modeloCocin.getElementAt(j).compareTo(s)==0){
-				return true;
-			}
-		}
-		return false;		
+
+	public void setModeloParkin(DefaultComboBoxModel<String> modeloParkin) {
+		this.modeloParkin = modeloParkin;
 	}
-	
-	public boolean estaSala(String s){
-		for (int j = 0; j<modeloSala.getSize();j++){
-			if(modeloSala.getElementAt(j).compareTo(s)==0){
-				return true;
-			}
-		}
-		return false;		
+
+	public DefaultComboBoxModel<String> getModeloSala() {
+		return modeloSala;
 	}
-	
-	public boolean estaDorm(String s){
-		for (int j = 0; j<modeloDorm.getSize();j++){
-			if(modeloDorm.getElementAt(j).compareTo(s)==0){
-				return true;
-			}
-		}
-		return false;		
+
+	public void setModeloSala(DefaultComboBoxModel<String> modeloSala) {
+		this.modeloSala = modeloSala;
 	}
-	
-	public boolean estaPark(String s){
-		for (int j = 0; j<modeloParkin.getSize();j++){
-			if(modeloParkin.getElementAt(j).compareTo(s)==0){
-				return true;
-			}
-		}
-		return false;		
+
+	public DefaultComboBoxModel<String> getModeloCocin() {
+		return modeloCocin;
+	}
+
+	public void setModeloCocin(DefaultComboBoxModel<String> modeloCocin) {
+		this.modeloCocin = modeloCocin;
+	}
+
+	public DefaultComboBoxModel<String> getModeloBanos() {
+		return modeloBanos;
+	}
+
+	public void setModeloBanos(DefaultComboBoxModel<String> modeloBanos) {
+		this.modeloBanos = modeloBanos;
+	}
+
+	public DefaultComboBoxModel<String> getModeloDorm() {
+		return modeloDorm;
+	}
+
+	public void setModeloDorm(DefaultComboBoxModel<String> modeloDorm) {
+		this.modeloDorm = modeloDorm;
 	}
 }
